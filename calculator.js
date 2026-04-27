5926eff34fcfe77805c22a94e0aa389396853884
Manoela Neves <manoelanevessr@gmail.com>
2026-04-27 18:26:06 -0300

    ✨ adiciona função de subtração

--- calculator.js
+++ calculator.js
@@ -5,9 +5,10 @@ function sum(v1, v2) {
 
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
-    // return v1 - v2;
+    return v1 - v2;
 }
 console.log(sum(5, 3));
+console.log(sub(5, 3));
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
